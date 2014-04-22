e53d994e0d637934060e6dafdcb36f97fbe09f42
hideack <hide.terai@gmail.com>
2014-04-22 09:49:23 +0900

    デプロイ用ユーザ名修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :branch, 'master'
 set :shared_paths, ['log']
 
 # Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
+set :user, 'remper'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 
 # This task is the environment that is loaded for most commands, such as
